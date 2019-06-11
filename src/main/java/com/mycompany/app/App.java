63c923683e11ff58ffafd6584e865e3a30282fca
Maximiliano Pretzel <maximiliano.pretzel@grupoesfera.com.ar>
2019-06-11 12:20:32 -0300

    Agrega http server hello world para prueba

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,11 @@
 package com.mycompany.app;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  */
@@ -10,8 +16,22 @@ public class App
 
     public App() {}
 
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
+    public static void main(String[] args) throws IOException {
+        //System.out.println(new App().getMessage());
+    	
+//    	HttpServer server = HttpServer.create(new InetSocketAddress(8000));
+//    	server.createContext("/applications/myapp", new MyHandler());
+//    	server.setExecutor(null); // creates a default executor
+//    	server.start();
+    	
+	    ServerSocket listener = new ServerSocket(8002);
+	    while(true){
+	      Socket sock = listener.accept();
+	      new PrintWriter(sock.getOutputStream(), true).
+	                println("Hola a Todos y Todas!");
+	      sock.close();
+	    }
+
     }
 
     private final String getMessage() {
---
+++ src/main/java/com/mycompany/app/MyHandler.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import com.sun.net.httpserver.spi.*;;
+
+class MyHandler /*implements HttpHandler*/ {
+//	   public void handle(HttpExchange t) throws IOException {
+//	       InputStream is = t.getRequestBody();
+//	       read(is); // .. read the request body
+//	       String response = "Hello World!";
+//	       t.sendResponseHeaders(200, response.length());
+//	       OutputStream os = t.getResponseBody();
+//	       os.write(response.getBytes()); os.close();
+//	   }
+	
+}
